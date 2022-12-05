185235ca6406e465c6776908ca305c1d27910984
David Marinho <david.marinho@codacy.com>
2022-12-05 12:11:49 +0000

    added js proj

--- js-proj/src/App.js
+++ js-proj/src/App.js
@@ -1,19 +1,15 @@
-import React, { Component } from "react";
-import "./App.css";
-/* eslint-disable no-unused-vars */
+import React, { Component } from 'react';
+import './App.css';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>A React app</h1>
-        <p>
-          Edit 
-          <code>
-            src/App.js
-          </code>, 
-          save and reload.
-        </p>
+        <p>Edit <code>src/App.js</code>, save and reload.</p>
       </div>
     );
   }
 }
+
+export default App;
