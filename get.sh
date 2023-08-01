766b5d9bec940b4b1fb8df0c8198c48192c0b748
David Marinho <david.marinho@codacy.com>
2023-08-01 11:32:47 +0100

    added get.sh

--- get.sh
+++ get.sh
@@ -133,7 +133,7 @@ download() {
 
     download_file "$url"
     checksum "$file_name" "$checksum_url"
-    mv "$file_name" "$output_filename"
+    mv -n "$file_name" "$output_filename"
 
     log "$output_filename"
 
