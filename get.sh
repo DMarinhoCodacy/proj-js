5217b772d8ea033db042dfbfbd8d9f73dbfb01ae
David Marinho <david.marinho@codacy.com>
2023-08-01 15:00:27 +0100

    added get.sh

--- get.sh
+++ get.sh
@@ -147,17 +147,12 @@ download_reporter() {
     local reporter_folder=$2
     local reporter_filename=$3
 
-    if [ ! -f "$reporter_path" ]
-    then
         log "$i" "Downloading the codacy reporter $binary_name... ($CODACY_REPORTER_VERSION)"
 
         binary_url="https://artifacts.codacy.com/bin/codacy-coverage-reporter/$CODACY_REPORTER_VERSION/$binary_name"
         checksum_url="https://github.com/codacy/codacy-coverage-reporter/releases/download/$CODACY_REPORTER_VERSION/$binary_name.SHA512SUM"
 
         download "$binary_url" "$binary_name" "$reporter_folder" "$reporter_filename" "$checksum_url"
-    else
-        log "$i" "Codacy reporter $binary_name already in cache"
-    fi
 }
 
 is_self_hosted_instance() {
