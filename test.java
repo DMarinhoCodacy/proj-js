5df2c5f93c1efb79e70a4edb531cc40e3d25b52d
David Marinho <david.marinho@codacy.com>
2024-01-17 21:25:43 +0000

    fix java file

--- test.java
+++ test.java
@@ -1,13 +1,26 @@
-public class checkstyle {
-    public void importantMethod() {
-        try {
-            throw new RuntimeException("error");
-        } catch (Exception ex) {
-            // ignore
+package com.codacy.utils;
+
+public class Math {
+
+    private int magicNumber;
+
+    public Math(int magicNumber) {
+        this.magicNumber = magicNumber;
+    }
+
+    /**
+     * Adds 2 numbers, unless there is a magic number on the second argument
+     */
+    public int magicAdd(int x, int y) {
+        if (y == magicNumber) {
+            System.out.println("Am I am covered?");
+            System.out.println("Am I am covered?");
+            System.out.println("Am I am covered?");
+            return y - x;
+        } else {
+            System.out.println("I am covered!");
+            System.out.println("I am covered!");
+            return x + y;
         }
     }
 }
-
-
-
-// this is a test
