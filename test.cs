2786a978b982d029bee517a92e61e7da9ae78418
David Marinho <david.marinho@codacy.com>
2024-01-24 14:43:34 +0000

    add cs file

---
+++ test.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using LSports.Core.Common.Extensions;
+using LSports.Data.Core.Legacy.ProviderBet;
+using LSports.Data.Core.Legacy.ProviderMarket;
+using LSports.RobotsCommon.Extensions;
+using LSports.RobotsFramework.Entities.Bases;
+using LSports.RobotsFramework.Entities.Models;
+using LSports.RobotsFramework.MarketHandlers.Bases;
+
+namespace LSports.Robots.ProvidersCommon.OneXBet.Logic.MarketHandlers
+{
+    public class TeamWinsCommonMarketHandler : SimpleMarketHandlerBase<MarketItem>
+    {
+        protected override List<string> MatchCases { get; }= new List<string> {"Team Wins"};
+
+
+        protected override ProviderBet HandleBetInternal(BetItemBase betItem)
+        {
+            betItem.Name = betItem.Name.Remove("Team","Wins");
+            return base.HandleBetInternal(betItem);
+        }
+
+        protected override bool TryAddBet(ProviderMarket providerMarket, ProviderBet providerBet)
+        {
+
+            if (string.IsNullOrEmpty(providerBet?.Name) || providerBet.Name.CompareContent("draw"))
+            {
+                return false;
+            }
+            return base.TryAddBet(providerMarket, providerBet);
+        }
+    }
+}
