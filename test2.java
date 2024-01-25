4dc3d3e1e13ea2bf07772ac1d3d93b214a10ec8f
David Marinho <david.marinho@codacy.com>
2024-01-25 10:29:18 +0000

    new-pr

---
+++ test2.java
@@ -0,0 +1,11 @@
+public class checkstyle {
+    public void importantMethod() {
+        try {
+            throw new RuntimeException("error");
+        } catch (Exception ex) {
+            // ignore
+        }
+    }
+}
+
+// this is a test
